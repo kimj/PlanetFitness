360a23cc366784f6a75f3ce394019280361e69d4
kimj <jinn.kim@gmail.com>
2026-09-11 15:23:31 -0400

    remove unused import

--- app/src/main/java/com/mentalmachines/planetfitness/features/programoverview/ProgramOverview.kt
+++ app/src/main/java/com/mentalmachines/planetfitness/features/programoverview/ProgramOverview.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
